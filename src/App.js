eb9a4f16f0e51513f735ab6c0ebb1a787a249613
Mallika <mpatil@uic.edu>
2020-08-10 20:49:08 -0500

    website

--- src/App.js
+++ src/App.js
@@ -27,93 +27,115 @@ import {
 function App() {
   return (
     <Layout>
-      // Header
-      <Header transparent title="Homeroom" class="header-color">
-        <Navigation>
-          <a href="#">About</a>
-          <a href="#">Features</a>
-          <a href="#">Stats</a>
-          <a href="#">Team</a>
-          <a href="#">Download Now</a>
-        </Navigation>
-      </Header>
       <Content>
-        <Card
-          shadow={100}
-          style={{
-            width: "50em",
-            margin: "auto",
-            marginTop: "50px",
-          }}
-        >
-          <CardTitle
-            style={{
-              color: "#fff",
-              height: "176px",
-              background:
-                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
-            }}
-          >
-            To Do List
-          </CardTitle>
-        </Card>
-        <Card
-          shadow={100}
-          style={{
-            width: "50em",
-            margin: "auto",
-            marginTop: "50px",
-          }}
-        >
-          <CardTitle
-            style={{
-              color: "#fff",
-              height: "176px",
-              background:
-                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
-            }}
-          >
-            To Do List
-          </CardTitle>
-          <CardText>
-            Keep track of your task list at the tip of your finger tips.{" "}
-          </CardText>
-          <CardActions border>
-            <Button colored>Get Started</Button>
-          </CardActions>
-          <CardMenu style={{ color: "#fff" }}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
-        <Card
-          shadow={75}
-          style={{
-            width: "50em",
-            margin: "auto",
-            marginTop: "50px",
-          }}
-        >
-          <CardTitle
-            style={{
-              color: "#fff",
-              height: "176px",
-              background:
-                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
-            }}
-          >
-            Welcome
-          </CardTitle>
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            sagittis pellentesque lacus eleifend lacinia...
-          </CardText>
-          <CardActions border>
-            <Button colored>Get Started</Button>
-          </CardActions>
-          <CardMenu style={{ color: "#fff" }}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
+        <Header class="header-color" title="Title" scroll>
+          <Navigation>
+            <a href="#">To Do</a>
+            <a href="#">Class Tasks</a>
+            <a href="#">Pomodoro Timer</a>
+            <a href="#">Website Blocker</a>
+          </Navigation>
+        </Header>
+        <Grid className="demo-grid-2" scroll>
+          <Cell col={12}>
+            <Card
+              scroll
+              style={{
+                width: "77em",
+                margin: "auto",
+              }}
+            >
+              <CardTitle
+                style={{
+                  color: "#fff",
+                  height: "176px",
+                  background:
+                    "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
+                }}
+              >
+                To Do List
+              </CardTitle>
+              <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                sagittis pellentesque lacus eleifend lacinia...
+              </CardText>
+            </Card>
+          </Cell>
+          <Cell col={4}>
+            <Card
+              shadow={0}
+              style={{
+                height: "300px",
+                width: "400px",
+                background:
+                  "url(https://media.discordapp.net/attachments/741774580204437589/742552304556834896/unknown.png) center / cover",
+                margin: "auto",
+              }}
+            ></Card>
+          </Cell>
+          <Cell col={4}>
+            <Card
+              shadow={0}
+              style={{
+                height: "300px",
+                width: "400px",
+                background:
+                  "url(https://media.discordapp.net/attachments/741774580204437589/742552777976316024/unknown.png) center / cover",
+                margin: "auto",
+              }}
+            ></Card>
+          </Cell>
+        </Grid>
+        <Grid className="demo-grid-2" scroll>
+          <Cell col={12}>
+            <Card
+              scroll
+              style={{
+                width: "77em",
+                margin: "auto",
+              }}
+            >
+              <CardTitle
+                style={{
+                  color: "#fff",
+                  height: "176px",
+                  background:
+                    "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
+                }}
+              >
+                To Do List
+              </CardTitle>
+              <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                sagittis pellentesque lacus eleifend lacinia...
+              </CardText>
+            </Card>
+          </Cell>
+          <Cell col={4}>
+            <Card
+              shadow={0}
+              style={{
+                height: "300px",
+                width: "400px",
+                background:
+                  "url(https://media.discordapp.net/attachments/741774580204437589/742552304556834896/unknown.png) center / cover",
+                margin: "auto",
+              }}
+            ></Card>
+          </Cell>
+          <Cell col={4}>
+            <Card
+              shadow={0}
+              style={{
+                height: "300px",
+                width: "400px",
+                background:
+                  "url(https://media.discordapp.net/attachments/741774580204437589/742552777976316024/unknown.png) center / cover",
+                margin: "auto",
+              }}
+            ></Card>
+          </Cell>
+        </Grid>
       </Content>
     </Layout>
   );
