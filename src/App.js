bce915e5ab42bc2168a36b954c7ef1bf0ebe3676
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-08-10 18:03:57 -0700

    site

--- src/App.js
+++ src/App.js
@@ -26,14 +26,101 @@ import {
 
 function App() {
   return (
-    <Layout>
-      <Header transparent title="Homeroom" class="header-color">
+    <Layout style={{backgroundColor: '#41B3A3'}}>
+      <Header transparent title="Title" style={{color: 'white'}}>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+        <Card
+          shadow={100}
+          style={{
+            width: "50em",
+            margin: "auto",
+            marginTop: "50px",
+          }}
+        >
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
+            }}
+          >
+            To Do List
+          </CardTitle>
+        </Card>
+        <Card
+          shadow={100}
+          style={{
+            width: "50em",
+            margin: "auto",
+            marginTop: "50px",
+          }}
+        >
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
+            }}
+          >
+            To Do List
+          </CardTitle>
+          <CardText>
+            Keep track of your task list at the tip of your finger tips.{" "}
+          </CardText>
+          <CardActions border>
+            <Button colored>Get Started</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+        <Card
+          shadow={75}
+          style={{
+            width: "50em",
+            margin: "auto",
+            marginTop: "50px",
+          }}
+        >
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(http://www.getmdl.io/assets/demos/welcome_card.jpg)",
+            }}
+          >
+            Welcome
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            sagittis pellentesque lacus eleifend lacinia...
+          </CardText>
+          <CardActions border>
+            <Button colored>Get Started</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      </Content>
+    </Layout>
+    
+    /*<Header transparent title="Homeroom" class="header-color">
+        <img src="homeroom.png"></img>
         <Navigation>
-          <a href="#">About</a>
-          <a href="#">Features</a>
-          <a href="#">Stats</a>
-          <a href="#">Team</a>
-          <a href="#">Download Now</a>
+          <a href="https://github.com/mchow101/productivity-extension/tree/todo">Github Code</a>
         </Navigation>
       </Header>
       <Content>
@@ -114,7 +201,7 @@ function App() {
           </CardMenu>
         </Card>
       </Content>
-    </Layout>
+    </Layout>*/
   );
 }
 
