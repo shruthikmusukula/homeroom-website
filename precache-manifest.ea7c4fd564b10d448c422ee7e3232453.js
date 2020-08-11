c1aa8149dcefe9649fc56f132d0ccfdbfb9f905e
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-08-10 19:53:39 -0700

    Updates

---
+++ precache-manifest.ea7c4fd564b10d448c422ee7e3232453.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b1abca8604af43b9e7894cc2f02c00",
+    "url": "/productivity-extension-website/index.html"
+  },
+  {
+    "revision": "1647c78d0b99617091c7",
+    "url": "/productivity-extension-website/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "d4faa899a6a82db7edc4",
+    "url": "/productivity-extension-website/static/css/main.3fa1273d.chunk.css"
+  },
+  {
+    "revision": "1647c78d0b99617091c7",
+    "url": "/productivity-extension-website/static/js/2.69cc956b.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/productivity-extension-website/static/js/2.69cc956b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4faa899a6a82db7edc4",
+    "url": "/productivity-extension-website/static/js/main.bb6efead.chunk.js"
+  },
+  {
+    "revision": "0e4c5f1c42af495d3448",
+    "url": "/productivity-extension-website/static/js/runtime-main.17d62425.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/productivity-extension-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
